31aa249a21e0509c02d090942747c8489c36f6c1
Ahmad_Maaz <146940373+Thecodingpm@users.noreply.github.com>
2026-08-28 21:13:08 +0500

    feat(firebase): synchronize latest google-services.json with official Google Play and iOS OAuth clients

--- expo-app/src/config/firebase.js
+++ expo-app/src/config/firebase.js
@@ -7,6 +7,6 @@ export const FIREBASE_CONFIG = {
   appId: '1:1065207297774:android:05bb11ae1b3e4b88ec25ff',
   storageBucket: 'lift-e44ad.firebasestorage.app',
   webClientId: '1065207297774-iobkj4faj2tm3qa4m4q1tsr20rj25a8q.apps.googleusercontent.com',
-  androidClientId: '1065207297774-c50mr9pt80uv0llaluv6qknrqtsl75qt.apps.googleusercontent.com',
+  androidClientId: '1065207297774-qvfgerg5an643v9jtg9uqeg9euefd80e.apps.googleusercontent.com',
   iosClientId: '1065207297774-3n7ma3r15k9a7nl4djcl3r6m8asnkb6p.apps.googleusercontent.com'
 };
